1998289095aa34639137f960d5598ffe4c64dbc6
Roman Bodnar <wizzardrince@gmail.com>
2024-11-25 23:23:44 +0100

    [any message]

--- src/main.cs
+++ src/main.cs
@@ -7,4 +7,25 @@ Console.WriteLine("Logs from your program will appear here!");
 // Uncomment this block to pass the first stage
 TcpListener server = new TcpListener(IPAddress.Any, 9092);
 server.Start();
-server.AcceptSocket(); // wait for client
+using var socket = server.AcceptSocket(); // wait for client
+
+
+// first I'll use a list which will be converted to an array
+// todo: use a struct
+int messageSize = sizeof(int) * 2;
+int correlationId = 7;
+var message = new List<int> {
+    messageSize,
+    correlationId
+};
+
+var messageArr = message.ToArray();
+byte[] buffer = new byte[messageArr.Length * sizeof(int)];
+Buffer.BlockCopy(messageArr, 0, buffer, 0, buffer.Length);
+
+await socket.SendAsync(buffer, 0);
+
+public struct Message {
+    public int MessageSize;
+    public int CorrelationId;
+}
